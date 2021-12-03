d3e3afcddabf91f97d8d3fe9ad85400b2e151998
Parth Navadiya <nparth@narola.local>
2021-12-03 15:44:53 +0530

    update database config

--- utils/database.js
+++ utils/database.js
@@ -51,7 +51,6 @@ function reconnect(connection) {
   console.log("\n New connection tentative...");
   if (connection) connection.destroy();
   var connection = mysql.createConnection(db_config);
-
   connection.connect(function (err) {
     if (err) {
       setTimeout(reconnect, 2000);
@@ -62,50 +61,50 @@ function reconnect(connection) {
   });
 }
 
-connection.on("error", function (err) {
-  if (err.code === "PROTOCOL_CONNECTION_LOST") {
-    console.log(
-      "/!\\ Cannot establish a connection with the database. /!\\ (" +
-        err.code +
-        ")"
-    );
-    connection = reconnect(connection);
-  }
+// connection.on("error", function (err) {
+//   if (err.code === "PROTOCOL_CONNECTION_LOST") {
+//     console.log(
+//       "/!\\ Cannot establish a connection with the database. /!\\ (" +
+//         err.code +
+//         ")"
+//     );
+//     connection = reconnect(connection);
+//   }
 
-  else if (err.code === "PROTOCOL_ENQUEUE_AFTER_QUIT") {
-    console.log(
-      "/!\\ Cannot establish a connection with the database. /!\\ (" +
-        err.code +
-        ")"
-    );
-    connection = reconnect(connection);
-  }
+//   else if (err.code === "PROTOCOL_ENQUEUE_AFTER_QUIT") {
+//     console.log(
+//       "/!\\ Cannot establish a connection with the database. /!\\ (" +
+//         err.code +
+//         ")"
+//     );
+//     connection = reconnect(connection);
+//   }
 
-  else if (err.code === "PROTOCOL_ENQUEUE_AFTER_FATAL_ERROR") {
-    console.log(
-      "/!\\ Cannot establish a connection with the database. /!\\ (" +
-        err.code +
-        ")"
-    );
-    connection = reconnect(connection);
-  }
+//   else if (err.code === "PROTOCOL_ENQUEUE_AFTER_FATAL_ERROR") {
+//     console.log(
+//       "/!\\ Cannot establish a connection with the database. /!\\ (" +
+//         err.code +
+//         ")"
+//     );
+//     connection = reconnect(connection);
+//   }
 
-  else if (err.code === "PROTOCOL_ENQUEUE_HANDSHAKE_TWICE") {
-    console.log(
-      "/!\\ Cannot establish a connection with the database. /!\\ (" +
-        err.code +
-        ")"
-    );
-  }
+//   else if (err.code === "PROTOCOL_ENQUEUE_HANDSHAKE_TWICE") {
+//     console.log(
+//       "/!\\ Cannot establish a connection with the database. /!\\ (" +
+//         err.code +
+//         ")"
+//     );
+//   }
 
-  else {
-    console.log(
-      "/!\\ Cannot establish a connection with the database. /!\\ (" +
-        err.code +
-        ")"
-    );
-    connection = reconnect(connection);
-  }
-});
+//   else {
+//     console.log(
+//       "/!\\ Cannot establish a connection with the database. /!\\ (" +
+//         err.code +
+//         ")"
+//     );
+//     connection = reconnect(connection);
+//   }
+// });
 
 module.exports = database === Default_database ? connection : mongodbConnect;
